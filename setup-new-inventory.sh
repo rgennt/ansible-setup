603af8c434c35a5cdb4601cdf35ba60abe260e3d
mfallone <m.fallone@psiphon.ca>
2019-03-25 12:01:42 -0400

    Added setup-new-inventory script to automate new inventory directory

---
+++ setup-new-inventory.sh
@@ -0,0 +1,17 @@
+#!/bin/bash 
+
+# setup-new-inventory.sh
+# Sets up a new inventory structure for ansible-playbooks
+
+ANSIBLE_INVENTORY_PATH="../ansible-inventory"
+ANSIBLE_INVENTORIES=("production" "testing")
+
+for inv in "${ANSIBLE_INVENTORIES[@]}"; do
+    echo "Creating inventory dir: ${ANSIBLE_INVENTORY_PATH}/${inv}"
+    mkdir -p ${ANSIBLE_INVENTORY_PATH} \
+         ${ANSIBLE_INVENTORY_PATH}/${inv} \
+         ${ANSIBLE_INVENTORY_PATH}/${inv}/host_vars \
+         ${ANSIBLE_INVENTORY_PATH}/${inv}/group_vars
+    
+    touch ${ANSIBLE_INVENTORY_PATH}/${inv}/hosts
+done
