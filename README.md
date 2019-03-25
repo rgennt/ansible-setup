a63964b0e672a3d2144fc87c60c9dee2cd9f923f
mfallone <m.fallone@psiphon.ca>
2019-03-25 08:23:19 -0400

    Added initial README and setup.sh file to download and install ansible on control machine

---
+++ README.md
@@ -0,0 +1,6 @@
+# README - ansible-setup
+
+This repository is used to download and set up the local environment for use
+with Ansible.
+
+This is adapted from the official Ansible [Installation Guide](https://docs.ansible.com/ansible/latest/installation_guide/intro_installation.html).
---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# This script must be run first on the intended Ansible control machine.
+
+if [ ${EUID} -ne 0 ]; then
+    echo "This script must be run as the root user"
+    exit 1
+fi
+
+OS_DISTRO=$(lsb_release -i | awk '{ print $3 }' | tr '[:upper:]' '[:lower:]')
+
+if [ ${OS_DISTRO} == 'ubuntu' ]; then
+    # Update apt, install prerequisites.  
+    # Install ansible ppa and install the ansible package.
+    apt update
+    apt install software-properties-common
+    apt-add-repository --yes --update ppa:ansible/ansible
+    apt install -y -f ansible
+elif [ ${OS_DISTRO} == 'debian' ]; then
+    apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
+    apt update
+    apt install -y -f ansible
+fi
